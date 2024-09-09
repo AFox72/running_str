d559ac5d09d9328c12a86115afbe52f8b209aa12
AFox72 <a.fox.04.06@gmail.com>
2024-09-09 20:43:29 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#!o_lzpmnuy--gdz!v!)!o^_omg-%bwx&=06+zv31pzf2o^rja
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
