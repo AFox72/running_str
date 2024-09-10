eb9cb41fdcea155fd7c60bfa7069e59c04ac8410
AFox72 <a.fox.04.06@gmail.com>
2024-09-10 20:42:22 +0300

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -11,7 +11,7 @@ def create_video_opencv(message: str):
     else:
         title = message
 
-    title = transliterate.slugify('ъ' + title)
+    title = transliterate.slugify(title)
     width, height = 100, 100
     out = cv2.VideoWriter(f"media/videos/{title}.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 24, (width, height))
     frame = np.zeros((height, width, 3), dtype=np.uint8)
